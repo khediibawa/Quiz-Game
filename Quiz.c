78d7b76a15b928d6c3113471f9646bdfbec436cb
fallenone13 <46295874+fallenone13@users.noreply.github.com>
2019-01-11 11:18:20 +0530

    defined rules() function and added comments

--- Quiz.c
+++ Quiz.c
@@ -45,6 +45,11 @@ void welcome() {
 }
 
 
+/*-----------------------------------
+ * Menu Screen
+ *-----------------------------------
+ */
+
 void menu() {
 	system("CLS"); //clears the screen
 
@@ -58,15 +63,15 @@ void menu() {
 	
 	switch(choice) {
 		case 1:
-			startGame();
+			startGame(); //starts the game
 			break;
 			
 		case 2:
-			rules();
+			rules(); //shows the rules
 			break;
 		
 		case 3:
-			system("cls");
+			system("cls"); //quits the program
 			return;
 			break;
 		
@@ -75,10 +80,53 @@ void menu() {
 	}
 }
 
+
+/*-----------------------------------
+ * Choice 1 = starts the game
+ *-----------------------------------
+ */
+
 void startGame() {
 		
 }
 
+
+/*-----------------------------------
+ * Choice 2 = shows the game rules
+ *-----------------------------------
+ */
+
 void rules() {
 	
+	printf("\n\n\t\t\t\t RULES \t\t\t");
+	printf("\n********************************************************************************\n");
+	printf("\n A question will be displayed with 4 options\n\n Press the key (A/B/C/D) corresponding to the correct answer according to you.\n\n For every right answer, you earn $100,000.\n\n If your answer is wrong, the game ends.\n\n Answer maximum number of questions correctly to achieve highest scores and\n become a millionaire\n");
+	printf("\n********************************************************************************\n\n\n");
+	
+	printf("Press 0 to return to the main menu\n");
+	printf("Press 1 to start the game\n");
+	printf("Press 2 to exit the game\n");
+	
+	short int i; //variable to select one of the three choices above
+	scanf("%hd",&i);
+	
+	switch(i) {
+		case 0 :
+			menu(); //displays the menu screen again
+			break;
+		
+		case 1 :
+			startGame(); //starts the game
+			break;
+		
+		case 2:
+			system("cls");
+			return;
+			break;
+			
+		default :
+			rules(); //switch will keep on opening the rules page unless you enter a valid option
+	}
 }
+
+
